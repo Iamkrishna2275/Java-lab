c2fcc25cfab4cee62dfefdcb3c7045aa89572101
Iamkrishna2275 <krishnakundan2275@gmail.com>
2022-08-25 11:51:59 +0530

    Pattern

---
+++ Pattern/src/Pattern/Pattern.java
@@ -0,0 +1,36 @@
+package Pattern;
+import java.util.*;//importing utility
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*program to print pattern by using user input
+		 * 1
+		 * 121
+		 * 1231
+		 */
+				int i, j, rows; 
+				Scanner sc=new Scanner(System.in);
+				System.out.println("enter the number of raw");
+				rows=sc.nextInt();
+				for (i = 1; i <= rows; i++)   
+				{  
+					//Prints first half of the row  
+					for (j = 1; j <= i; j++)   
+					{   
+						System.out.print(j+" ");   
+					}  
+					//Prints second half of the row   
+					for (j = i-1; j >= 1; j--)  
+					{  
+						System.out.print(j+" ");  
+					}  
+					System.out.println();  
+
+				}
+
+			}  
+		
+		}
+
+
